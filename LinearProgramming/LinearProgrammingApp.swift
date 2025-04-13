5fff581a1c067e3062b82f1b44e4cdf3079377e2
Alexander Pleshakov <sanya.pleshakov.95@mail.ru>
2025-04-13 21:29:37 +0300

    Initial Commit

---
+++ LinearProgramming/LinearProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearProgrammingApp.swift
+//  LinearProgramming
+//
+//  Created by Александр Плешаков on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LinearProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
